2d87884b12ec97b83fb5f0888743aca1335b89b9
wgmin0416 <min20000416@naver.com>
2023-03-22 15:08:32 +0900

    [23.03.22] frontend 기본 컴포넌트 추가

---
+++ frontend/src/components/body/body.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Body() {
+    return (
+        <footer>
+            <h1>Body</h1>
+        </footer>
+    );
+}
+
+export default Body;
---
+++ frontend/src/components/bottom/bottom.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Bottom() {
+    return (
+        <footer>
+            <h1>Bottom</h1>
+        </footer>
+    );
+}
+
+export default Bottom;
