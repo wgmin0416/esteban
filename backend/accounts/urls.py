8547e24324b85cd2eb32404e3d0522ad177464a4
DevReady <amsmutu@gmail.com>
2023-06-11 16:45:59 +0900

    230611 api button for user list

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', views.login_view, name='login'),
+    path('json/', views.json_api, name='json_api'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,7 +1,40 @@
-# from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+
+# Create jons reaction
+# def json_api(request):
+#     data = {'message': 'Hello, world!'}
+#     return JsonResponse(data, status=200)
+
+def json_api(request):
+    
+    # 기본모델 auth_user
+    users = User.objects.all()
+
+    # 커스텀모델 사용시 - db 구조 개선필요
+    # users = community_user.objects.all()
+
+    user_list = []
+    for user in users:
+
+        user_list.append({
+            # auth user
+            'id': user.id,
+            'name': user.username,
+            'date_joined': user.date_joined
+
+            # community_user
+            # 'user_id': user.user_id,
+            # 'user_name': user.user_name,
+            # 'user_email': user.user_email,
+            # 'created_at': user.created_at,
+            # 'updated_at': user.updated_at
+        })
+    data = {'users': user_list}
+    return JsonResponse(data, status=200)
 
 # Create your views here.
 def hello(request):
@@ -34,3 +67,4 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('accounts:hello')
+
