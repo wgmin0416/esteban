229d70f9ad8ae2f4a2355909127b14d99eb5ce62
wgmin0416 <min20000416@naver.com>
2025-03-11 23:02:40 +0900

    sequelize 인스턴스 변수 환경변수로 변경

--- backend/db/sequelize.js
+++ backend/db/sequelize.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
 
 // Sequelize 인스턴스 생성
-const sequelize = new Sequelize("esteban", "root", "Alsdndrl1!", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: true,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    logging: true,
+  }
+);
 
 module.exports = sequelize;
