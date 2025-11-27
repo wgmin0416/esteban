97bb3cdb953dda0fd0c2abb29b77521df5f3497f
wgmin <min20000416@naver.com>
2025-11-27 19:22:06 +0900

    Feat: 프로필 페이지 수정 기능 추가
    - frontend
    1. 프로필 페이지 수정 기능을 위한 state값과 handle 함수 추가

--- frontend/src/pages/user/ProfilePage.jsx
+++ frontend/src/pages/user/ProfilePage.jsx
@@ -1,23 +1,74 @@
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 import useAuthStore from '../../store/useAuthStore';
+import { useState } from 'react';
 
 const ProfilePage = () => {
   const myInfo = useAuthStore((state) => state.myInfo);
-  const getMyInfo = useAuthStore((state) => state.getMyInfo);
-  useEffect(() => {
-    getMyInfo();
-  }, []);
+  const [updateInfo, setUpdateInfo] = useState({
+    id: myInfo.id,
+    name: myInfo.name,
+    email: myInfo.email,
+    phone: myInfo.phone,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUpdateInfo({
+      ...updateInfo,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault;
+    console.log('제출 데이터: ', updateInfo);
+  };
   return (
     <>
       <div>
-        <Link to="/">Home</Link>
-        <h1>ProfilePage</h1>
-        <p>This is the Profile page.</p>
-        <p>id: {myInfo.id}</p>
-        <p>name: {myInfo.name}</p>
-        <p>email: {myInfo.email}</p>
-        <p>phone: {myInfo.phone}</p>
+        <label>
+          ID:
+          <input
+            type="text"
+            name="id"
+            value={updateInfo.id ? updateInfo.id : ''}
+            disabled={true}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={updateInfo.name ? updateInfo.name : ''}
+            disabled={true}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={updateInfo.email ? updateInfo.email : ''}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Phone:
+          <input
+            type="tel"
+            name="phone"
+            value={updateInfo.phone ? updateInfo.email : ''}
+            onChange={handleChange}
+          />
+        </label>
+        <button onClick={handleSubmit}>수정</button>
       </div>
     </>
   );
