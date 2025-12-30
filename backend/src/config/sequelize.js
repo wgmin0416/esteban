fbfbac48c1e9a0523af1cb5f630278dc4eb71475
wgmin <min20000416@naver.com>
2025-12-30 20:13:40 +0900

    Feat: local 개발환경 추가
    NODE_ENV local 추가
    sequelize config 내 local 키 추가

--- backend/src/config/sequelize.js
+++ backend/src/config/sequelize.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
 
 const sequelizeConfig = {
+  local: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+    migrationStoragePath: 'src/migrations',
+    seederStoragePath: 'src/seeders',
+  },
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
